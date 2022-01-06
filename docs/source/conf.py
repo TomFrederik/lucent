8cfb6d037c8af872ef6bf968b4ed58d1ffa48a25
Tom Lieberum <50806732+TomFrederik@users.noreply.github.com>
2022-01-06 16:33:56 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,5 +1,10 @@
 # Configuration file for the Sphinx documentation builder.
 
+# -- sys path
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join('..', '..')))
+
 # -- Project information
 
 project = 'Lucent'
