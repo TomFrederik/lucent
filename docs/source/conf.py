9a975f63fe42b3e161942dddb9cdea34c51dd399
Tom Lieberum <50806732+TomFrederik@users.noreply.github.com>
2022-01-10 17:28:51 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,6 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
-# sys.path.insert(0, os.path.abspath('.'))
 
 
 # -- Project information -----------------------------------------------------
@@ -45,7 +44,6 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
-# Overwriting sphinx's default master doc
 master_doc = 'index'
 
 
