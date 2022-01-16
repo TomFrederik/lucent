3d7ccda76fdcec03cfbaeaab7b95b546687547eb
Tom Frederik <tlieberum@outlook.de>
2022-01-16 13:20:43 +0100

    Removed superfluous helper layers

--- lucent/modelzoo/inceptionv1/redirect_relu.py
+++ lucent/modelzoo/inceptionv1/redirect_relu.py
@@ -33,31 +33,6 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-
-class AdditionLayer(nn.Module):
-    def forward(self, t_1, t_2):
-        return t_1 + t_2
-
-
-class MaxPool2dLayer(nn.Module):
-    def forward(self, tensor, kernel_size=(3, 3), stride=(1, 1),
-                padding=0, ceil_mode=False):
-        return F.max_pool2d(tensor, kernel_size, stride=stride,
-                            padding=padding, ceil_mode=ceil_mode)
-
-
-class PadLayer(nn.Module):
-    def forward(self, tensor, padding=(1, 1, 1, 1), value=None):
-        if value is None:
-            return F.pad(tensor, padding)
-        return F.pad(tensor, padding, value=value)
-
-
-class ReluLayer(nn.Module):
-    def forward(self, tensor):
-        return F.relu(tensor)
-
-
 class RedirectedReLU(torch.autograd.Function):
     """
     A workaround when there is no gradient flow from an initial random input
@@ -76,35 +51,6 @@ class RedirectedReLU(torch.autograd.Function):
         grad_input[input_tensor < 0] = grad_input[input_tensor < 0] * 1e-1
         return grad_input
 
-
 class RedirectedReluLayer(nn.Module):
     def forward(self, tensor):
         return RedirectedReLU.apply(tensor)
-
-
-class SoftMaxLayer(nn.Module):
-    def forward(self, tensor, dim=1):
-        return F.softmax(tensor, dim=dim)
-
-
-class DropoutLayer(nn.Module):
-    def forward(self, tensor, p=0.4000000059604645, training=False, inplace=True):
-        return F.dropout(input=tensor, p=p, training=training, inplace=inplace)
-
-
-class CatLayer(nn.Module):
-    def forward(self, tensor_list, dim=1):
-        return torch.cat(tensor_list, dim)
-
-
-class LocalResponseNormLayer(nn.Module):
-    def forward(self, tensor, size=5, alpha=9.999999747378752e-05, beta=0.75, k=1.0):
-        return F.local_response_norm(tensor, size=size, alpha=alpha, beta=beta, k=k)
-
-
-class AVGPoolLayer(nn.Module):
-    def forward(self, tensor, kernel_size=(7, 7), stride=(1, 1), padding=(0,),
-                ceil_mode=False, count_include_pad=False):
-        return F.avg_pool2d(tensor, kernel_size=kernel_size, stride=stride,
-                            padding=padding, ceil_mode=ceil_mode,
-                            count_include_pad=count_include_pad)
