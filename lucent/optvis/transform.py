0577fe867c81117e9745f8b91cfb07aedb0106bc
Tom Frederik <tlieberum@outlook.de>
2022-01-19 18:17:42 +0100

    Fix typevar import

--- lucent/optvis/transform.py
+++ lucent/optvis/transform.py
@@ -15,7 +15,7 @@
 
 from __future__ import absolute_import, division, print_function
 
-from typing import Callable, Optional, Iterable
+from typing import Callable, Optional, Iterable, TypeVar
 
 import kornia
 from kornia.geometry.transform import translate
