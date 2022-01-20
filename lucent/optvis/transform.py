336b1bf55ade0b5839b5d5366686ae28b45e757e
Tom Frederik <tlieberum@outlook.de>
2022-01-20 11:29:09 +0100

    fix imports

--- lucent/optvis/transform.py
+++ lucent/optvis/transform.py
@@ -15,7 +15,7 @@
 
 from __future__ import absolute_import, division, print_function
 
-from typing import Callable, Optional, Iterable, Union
+from typing import Callable, Optional, Iterable, Union, List, Tuple
 
 import kornia
 from kornia.geometry.transform import translate
